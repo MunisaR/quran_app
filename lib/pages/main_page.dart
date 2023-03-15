9ad63a3f592def91b6b8c937c5077aeffaaa0d84
MunisaR <munisrahmatullayeva04@gmail.com>
2023-03-15 17:13:14 +0500

    feat: added main_pages

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quran_app/widgets/card_view.dart';
+import 'package:quran_app/widgets/circle_card.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-      ),
-    );
+    return CardView();
   }
 }
