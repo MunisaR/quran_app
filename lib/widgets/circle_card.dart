4bfdb3a5a864299d4880917f62a339b7352fa6fe
MrHumble21 <amalikov3@students.wiut.uz>
2023-03-16 10:10:17 +0500

    feat: commited

--- lib/widgets/circle_card.dart
+++ lib/widgets/circle_card.dart
@@ -10,42 +10,52 @@ class CircleCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // print(MediaQuery.of(context).size.width);
-    return Column(
-
-      children: [
-        Card(
-            elevation: 20,
-
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(150),
-            ),
-            child: Container(
-              // color: Colors.red,
-                width: MediaQuery.of(context).size.width <= 375 ? 120 : 81,
-                height:  MediaQuery.of(context).size.width <= 375 ? 120 : 81,
-                decoration: BoxDecoration(
-                    // The child of a round Card should be in round shape
-                    shape: BoxShape.circle,
-                    color: custom_grey),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Image.asset(
-                    customIcon,
-                    width: 30,
-                  ),
-                ))),
-        Text(
-          text,
-          textAlign: TextAlign.center,
-          overflow: TextOverflow.fade,
-          style: const TextStyle(
-            fontSize: 12.0,
-            fontFamily: 'Roboto',
-            color: Color(0xFF212121),
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
           ),
-        )
-      ],
+        );
+      },
+      child: Column(
+
+        children: [
+          Card(
+              elevation: 20,
+
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(150),
+              ),
+              child: Container(
+                // color: Colors.red,
+                  width: MediaQuery.of(context).size.width <= 375 ? 120 : 81,
+                  height:  MediaQuery.of(context).size.width <= 375 ? 120 : 81,
+                  decoration: BoxDecoration(
+                      // The child of a round Card should be in round shape
+                      shape: BoxShape.circle,
+                      color: custom_grey),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      customIcon,
+                      width: 30,
+                    ),
+                  ))),
+          Text(
+            text,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.fade,
+            style: const TextStyle(
+              fontSize: 12.0,
+              fontFamily: 'Roboto',
+              color: Color(0xFF212121),
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
