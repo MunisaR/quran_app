687fbd2726b60ad44889d02d39cc78928e9d74a9
MunisaR <munisrahmatullayeva04@gmail.com>
2023-03-24 18:03:53 +0500

    fear added praying

--- lib/pages/SurahPage.dart
+++ lib/pages/SurahPage.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 late  int i;
